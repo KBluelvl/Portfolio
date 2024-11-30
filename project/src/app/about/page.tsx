1b1996b8240c51bf7c61218c9699bc4a3dfadecb
KBluelvl <kbluelv1@gmail.com>
2024-11-30 22:19:32 +0100

    color + escaped

--- project/src/app/about/page.tsx
+++ project/src/app/about/page.tsx
@@ -1,4 +1,5 @@
 export default function about() {
     return (<>
+        <h1>Ceci est une page pour en apprendre plus </h1>
     </>)
 }
--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -3,22 +3,26 @@ import Link from "next/link";
 export default function Home() {
   return (<>
       <header>
-        <div className="fixed border border-[#33353F] top-0 left-0 right-0">
-          <div className="end">
-            <ul className="flex ">
-              <li><Link href="/">Home</Link></li> 
-              <li><Link href="/">Projects</Link></li>
-              <li><Link href="/about">About</Link></li>
-              <li><Link href="/about">Language</Link></li>
-            </ul>
+        <nav className="fixed border border-[#0e1845] top-0 left-0 right-0">
+          <div className="flex justify-between">
+            <h1 className="text-2xl my-3 ml-5 font-bold bg-gradient-to-r from-sky-500 to-indigo-500 text-transparent bg-clip-text">PORTFOLIO</h1>
+            <div className="my-3">
+              <ul className="flex justify-between text-2xl md:space-x-20 ">
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/projects">Projects</Link></li>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="https://github.com/KBluelvl/Portofolio" target="_blank">code</Link></li>
+                <li>Language</li>
+              </ul>
+            </div>
           </div>
-        </div>
+        </nav>
       </header>
 
       <main>
         <div className="container mt-24 px-12">
           <h1 className="text-5xl sm:text-7xl font-semibold">
-            <span className="text-blue-300">Hello, I'm</span>
+            <span className="text-blue-300">Hello, I&apos;m</span>
             <div className="font-semibold tracking-tight">
               Florian Essomba
             </div>
