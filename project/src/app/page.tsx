d8faa9f22538a62bdf222e36ee0e6d6ca4b3502e
KBluelvl <kbluelv1@gmail.com>
2024-11-30 17:35:53 +0100

    display name

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,11 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Florian Essomba</h1>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+  return (<>
+      <header>
+        <div className="fixed border border-[#33353F] top-0 left-0 right-0">
+          <div className="end">
+            <ul className="flex ">
+              <li><Link href="/">Home</Link></li> 
+              <li><Link href="/">Projects</Link></li>
+              <li><Link href="/about">About</Link></li>
+              <li><Link href="/about">Language</Link></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mt-24 px-12">
+          <h1 className="text-5xl sm:text-7xl font-semibold">
+            <span className="text-blue-300">Hello, I'm</span>
+            <div className="font-semibold tracking-tight">
+              Florian Essomba
+            </div>
+          </h1>
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
-    </div>
-  );
+
+      <footer></footer>
+      </>
+  )
 }
