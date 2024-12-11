b26f376597376d4d15b8011400e18c91c6e7a80b
KBluelvl <kbluelv1@gmail.com>
2024-12-11 15:29:40 +0100

    debug

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header(){
     return (
         <header>
           <nav className="fixed border border-[#0e1845] top-0 left-0 right-0 bg-background">
-            <div className="flex justify-between mx-5">
+            <div className="flex justify-between px-5">
               <Logo />
                 <ul className="flex sm:text-2xl space-x-20 max-[720px]:space-x-5 font-semibold">
                   <li className="flex items-center"><Link href="/">Home</Link></li>
