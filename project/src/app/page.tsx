8e2d0cd359bfc83aa4466d2dc2ba05f7f58e4a64
KBluelvl <kbluelv1@gmail.com>
2024-12-11 14:03:08 +0100

    showing flag name + logo link

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import { useState } from "react";
 
 export default function Home() {
+  const [isVisible, setVisible] = useState(false);
+
   return (<>
           <h1 className="md:text-5xl sm:text-7xl font-semibold">
             <span className="bg-gradient-to-r from-blue-600 to-sky-600 text-transparent bg-clip-text">Hello, I&apos;m</span>
@@ -17,7 +22,12 @@ export default function Home() {
             <Link href="https://www.linkedin.com/in/florian-essomba-804b1b216/">
               <Image src="/image/linkedin.jpg" width={45} height={45} className="rounded-full"  alt="Linkedin logo" />
             </Link>
-            <Image src="/image/belgium-flag.png" width={50} height={45} className="border rounded" alt="Belgium flag"/>
+            <div className="flex flex-col">
+              <Image src="/image/belgium-flag.png" width={50} height={45} className="border rounded" alt="Belgium flag"
+                onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}/>
+                {!isVisible && (<div className="hiddenFlagName">Belgium</div>)}
+                {isVisible && (<div className="flagName rounded">Belgium</div>)}
+             </div>
           </div>
           <Skills />
           <h1 className="text-2xl sm:text-1xl font-semibold mt-20 mb-20 text-center">Projects</h1>
--- project/src/components/Logo.tsx
+++ project/src/components/Logo.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link"
+
 export default function Logo() {
     return (
-        <h1 className="text-2xl my-3 ml-5 font-bold bg-gradient-to-r from-sky-500 to-indigo-500 text-transparent bg-clip-text">PORTFOLIO</h1>
+        <Link href="/">
+            <h1 className="text-2xl my-3 ml-5 font-bold bg-gradient-to-r from-sky-500 to-indigo-500 text-transparent bg-clip-text">PORTFOLIO</h1>
+        </Link>
     )
 }
