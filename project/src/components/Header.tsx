8a3848eee7706dd504720b621c0b749814375b5a
KBluelvl <kbluelv1@gmail.com>
2024-12-11 14:28:39 +0100

    center vertically header

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -7,14 +7,12 @@ export default function Header(){
           <nav className="fixed border border-[#0e1845] top-0 left-0 right-0 bg-background">
             <div className="flex justify-between">
               <Logo />
-              <div className="my-3">
-                <ul className="flex justify-between sm:text-2xl space-x-20 max-[720px]:space-x-5 mr-5 font-semibold">
-                  <li><Link href="/">Home</Link></li>
-                  <li><Link href="/projects">Projects</Link></li>
-                  <li><Link href="/about">About</Link></li>
-                  <li><Link href="https://github.com/KBluelvl/Portfolio" target="_blank">Code</Link></li>
+                <ul className="flex sm:text-2xl space-x-20 max-[720px]:space-x-5 mr-5 font-semibold">
+                  <li className="flex items-center"><Link href="/">Home</Link></li>
+                  <li className="flex items-center"><Link href="/projects">Projects</Link></li>
+                  <li className="flex items-center"><Link href="/about">About</Link></li>
+                  <li className="flex items-center"><Link href="https://github.com/KBluelvl/Portfolio" target="_blank">Code</Link></li>
                 </ul>
-              </div>
             </div>
           </nav>
         </header>
