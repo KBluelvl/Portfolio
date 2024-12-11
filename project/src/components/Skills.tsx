4665948f69f4251b02b336b30d0162757b4079f7
KBluelvl <kbluelv1@gmail.com>
2024-12-11 14:43:49 +0100

    responsive icons

--- project/src/components/Skills.tsx
+++ project/src/components/Skills.tsx
@@ -6,43 +6,43 @@ export default function Skills() {
           <div className="grid grid-cols-5 gap-1">
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/java.svg" width="32" height="32" className="mx-auto" alt="Java logo"/>
-              <p className="text-center">Java</p>
+              <p className="text-center max-w-[450px]:text-xs">Java</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/javascript.svg" width="32" height="32" className="mx-auto" alt="Javascript logo"/>
-              <p className="text-center">Javascript</p>
+              <p className="text-center max-w-[450px]:text-xs">Javascript</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/cpp.svg" width="32" height="32" className="mx-auto" alt="C++ logo"/>
-              <p className="text-center">C++</p>
+              <p className="text-center max-w-[450px]:text-xs">C++</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/c.svg" width="32" height="32" className="mx-auto" alt="C logo"/>
-              <p className="text-center">C</p>
+              <p className="text-center max-w-[450px]:text-xs">C</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/laravel.svg" width="32" height="32" className="mx-auto" alt="Laravel logo"/>
-              <p className="text-center">Laravel</p>
+              <p className="text-center max-w-[450px]:text-xs">Laravel</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/android.svg" width="32" height="32" className="mx-auto" alt="Android logo"/>
-              <p className="text-center">Android Studio</p>
+              <p className="text-center max-w-[450px]:text-xs">Android Studio</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/django.svg" width="32" height="32" className="mx-auto" alt="Django logo"/>
-              <p className="text-center">Django</p>
+              <p className="text-center max-w-[450px]:text-xs">Django</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/spring.svg" width="32" height="32" className="mx-auto" alt="Spring logo"/>
-              <p className="text-center">Spring</p>
+              <p className="text-center max-w-[450px]:text-xs">Spring</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/next-js.svg" width="32" height="32" className="mx-auto" alt="Next JS logo"/>
-              <p className="text-center">Next JS</p>
+              <p className="text-center max-w-[450px]:text-xs">Next JS</p>
             </div>
             <div className="hover:bg-gray-700">
               <Image src="/image/icon/typescript.svg" width="32" height="32" className="mx-auto" alt="Typescript logo"/>
-              <p className="text-center">Typescript</p>
+              <p className="text-center max-w-[450px]:text-xs">Typescript</p>
             </div>
           </div>
     </>)
