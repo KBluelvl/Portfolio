e574b4fe48014b635456ab92a31081762b75468b
KBluelvl <kbluelv1@gmail.com>
2024-12-11 14:31:51 +0100

    debug margin header

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -5,9 +5,9 @@ export default function Header(){
     return (
         <header>
           <nav className="fixed border border-[#0e1845] top-0 left-0 right-0 bg-background">
-            <div className="flex justify-between">
+            <div className="flex justify-between mx-5">
               <Logo />
-                <ul className="flex sm:text-2xl space-x-20 max-[720px]:space-x-5 mr-5 font-semibold">
+                <ul className="flex sm:text-2xl space-x-20 max-[720px]:space-x-5 font-semibold">
                   <li className="flex items-center"><Link href="/">Home</Link></li>
                   <li className="flex items-center"><Link href="/projects">Projects</Link></li>
                   <li className="flex items-center"><Link href="/about">About</Link></li>
