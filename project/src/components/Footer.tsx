fa6915afd18e45a184f64f4122cabf8d64a76e8b
KBluelvl <kbluelv1@gmail.com>
2024-12-14 14:29:47 +0100

    commentaire icon + tech projects + padding footer

--- project/src/components/Footer.tsx
+++ project/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 
 export default function Footer() {
     return (
-        <footer className="w-full mt-24 flex min-h-16 border border-[#0e1845] justify-between items-center max-[640px]:text-xs">
+        <footer className="w-full mt-24 flex min-h-16 border border-[#0e1845] justify-between items-center max-[640px]:text-xs px-2">
             <Logo />
             <div className="flex">
                 <p className="mr-5">This site was made with Tailwind CSS.</p>
--- project/src/components/Projects.tsx
+++ project/src/components/Projects.tsx
@@ -10,6 +10,20 @@ export default function Projects() {
             <div className="flex flex-col items-center space-y-2">
                 <Image src="/image/projects/STIB.png" width="597" height="298" className="rounded" alt="STIB project image" />
                 <h1 className="text-4xl text-center font-bold mt-4">STIB Path Finder</h1>
+                <div className="flex space-x-2">
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-[#E76F00]">
+                        <p>Java</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-[#5382A1]">
+                        <p>JavaFX</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-blue-900">
+                        <p>SQL</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-slate-700">
+                        <p>Dijkstra&apos;s algorithm</p>
+                    </h1>
+                </div>
                 <Link href="https://github.com/KBluelvl/STIB" target="_blank">
                     <h1 className="flex items-center justify-center space-x-2 border rounded-md px-1">
                         <p>Code</p>
@@ -21,6 +35,17 @@ export default function Projects() {
             <div className="flex flex-col items-center mt-10 space-y-2">
                 <Image src="/image/projects/StartReview.png" width="597" height="298" className="rounded" alt="StartReview project image" />
                 <h1 className="text-4xl text-center font-bold mt-4">StartReview</h1>
+                <div className="flex space-x-2">
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-blue-500">
+                        <p>Mobile</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-blue-900">
+                        <p>API</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-blue-900">
+                        <p>GraphQL</p>
+                    </h1>
+                </div>
                 <Link href="https://github.com/KBluelvl/StartReview" target="_blank">
                     <h1 className="flex items-center justify-center space-x-2 border rounded-md px-1">
                         <p>Code</p>
@@ -32,6 +57,14 @@ export default function Projects() {
             <div className="flex flex-col items-center mt-10 space-y-2">
                 <AutoPlayVideo />
                 <h1 className="text-4xl text-center font-bold mt-4">Chess</h1>
+                <div className="flex space-x-2">
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-[#E76F00]">
+                        <p>Java</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 border rounded-md border-none px-1 bg-[#5382A1]">
+                        <p>JavaFX</p>
+                    </h1>
+                </div>
                 <Link href="https://github.com/KBluelvl/Chess" target="_blank">
                     <h1 className="flex items-center justify-center space-x-2 border rounded-md px-1">
                         <p>Code</p>
@@ -43,6 +76,17 @@ export default function Projects() {
             <div className="flex flex-col items-center mt-10 space-y-2">
                 <Image src="/image/projects/Sokoban.png" width="597" height="298" className="rounded" alt="StartReview project image" />
                 <h1 className="text-4xl text-center font-bold mt-4">Sokoban</h1>
+                <div className="flex space-x-2">
+                    <h1 className="flex items-center justify-center space-x-2 px-1 border rounded-md border-none bg-[#d8c026]">
+                        <p>JavaScript</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 px-1 border rounded-md border-none bg-orange-600">
+                        <p>HTML</p>
+                    </h1>
+                    <h1 className="flex items-center justify-center space-x-2 px-1 border rounded-md border-none bg-blue-600">
+                        <p>CSS</p>
+                    </h1>
+                </div>
                 <div className="flex space-x-3">
                     <Link href="https://github.com/KBluelvl/Sokoban" target="_blank">
                         <h1 className="flex items-center justify-center space-x-2 border rounded-md px-1">
