f2a4f63fbab52b0c7f5add96e5939f4ce516d901
KBluelvl <kbluelv1@gmail.com>
2024-12-18 22:34:02 +0100

    debug

--- project/src/app/projects/[tag]/page.tsx
+++ project/src/app/projects/[tag]/page.tsx
@@ -1,14 +1,12 @@
 import projects from '@/data/projects';
 import ProjectCard from "@/components/ProjectCard";
 
-export default async function tag({params}: {params : {tag: string}}) {
-    const filteredProjects = projects.filter((project) => 
-        project.tags.includes(params.tag)
-    );
-    
+export default function TagPage({params}: {params: {tag: string}}) {
+    const { tag } = params;
+    const filteredProjects = projects.filter((project) => project.tags.includes(tag))
     return (
          <div className="flex flex-col items-center">
-            <h1 className="text-3xl">Projects built with <b>{params.tag}</b></h1>
+            <h1 className="text-3xl">Projects built with <b>{tag}</b></h1>
             {filteredProjects.map((project) =>{
                 return <ProjectCard key={project.id} project={project} />
             })}
