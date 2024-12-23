69fbd27051f8cc3f2aab2588c9a42a0eb57df232
KBluelvl <kbluelv1@gmail.com>
2024-12-23 14:10:42 +0100

    responsive tailwind logo

--- project/src/components/Footer.tsx
+++ project/src/components/Footer.tsx
@@ -5,12 +5,14 @@ export default function Footer() {
     return (
         <footer className="w-full mt-24 flex border border-[#0e1845] justify-between items-center max-[640px]:text-xs px-2">
             <Logo />
-            <div className="flex">
+            <div className="flex items-center">
                 <p className="text-gray-500">MADE WITH</p>
-                <Image src="/image/icon/tailwind-css.svg" width="32" height="32" className="px-2" alt="Tailwind logo" />
+                <div className="relative h-8 w-8 max-[640px]:h-2">
+                    <Image src="/image/icon/tailwind-css.svg" fill className="px-2 max-[640px]:px-0" objectFit="contain" alt="Tailwind logo" />
+                </div>
                 <p>Tailwind CSS</p>
             </div>
             <p>All rights reserved.</p>
         </footer>
     );
-}
+}   
