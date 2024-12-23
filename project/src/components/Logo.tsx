505c4e451ed8bdb114923713bb9ad36701bb9cac
KBluelvl <kbluelv1@gmail.com>
2024-12-23 14:37:34 +0100

    debug footer

--- project/src/components/Logo.tsx
+++ project/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Logo() {
     return (
         <Link href="/">
-            <h1 className="text-2xl my-3 font-bold bg-gradient-to-r from-sky-500 to-indigo-500 text-transparent bg-clip-text">PORTFOLIO</h1>
+            <h1 className="text-2xl max-[640px]:text-xs my-3 font-bold bg-gradient-to-r from-sky-500 to-indigo-500 text-transparent bg-clip-text">PORTFOLIO</h1>
         </Link>
     )
 }
