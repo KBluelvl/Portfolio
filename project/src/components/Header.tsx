8e746d02a5e9d5791a9a119570e0612fe02dd5f6
KBluelvl <kbluelv1@gmail.com>
2025-01-14 22:39:45 +0100

    hover in header

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -8,9 +8,9 @@ export default function Header(){
             <div className="flex justify-between px-5">
               <Logo />
                 <ul className="flex sm:text-2xl space-x-20 max-[720px]:space-x-5 font-semibold">
-                  <li className="flex items-center"><Link href="/projects">Projects</Link></li>
-                  <li className="flex items-center"><Link href="/about">About</Link></li>
-                  <li className="flex items-center"><Link href="/contact">Contact</Link></li>
+                  <li className="flex items-center"><Link href="/projects" className="hover:scale-110">Projects</Link></li>
+                  <li className="flex items-center"><Link href="/about" className="hover:scale-110">About</Link></li>
+                  <li className="flex items-center"><Link href="/contact" className="hover:scale-110">Contact</Link></li>
                 </ul>
             </div>
           </nav>
